64117ae295c9952fa8a89eabfaaf14346c0fc3c8
Holger Grosse-Plankermann <holgergp@gmail.com>
2017-09-22 16:23:32 +0200

    babel-preset-env

--- jestTalkApp/webpack.config.js
+++ jestTalkApp/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'env']
         }
       }
     ]
