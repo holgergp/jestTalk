e1b33942f189cc494d4bcd31f42cfc7a35b6c55a
Holger Grosse-Plankermann <holgergp@gmail.com>
2018-03-11 17:22:24 +0100

    Watchmode

--- jestTalkApp/client/service/serviceUser.js
+++ jestTalkApp/client/service/serviceUser.js
@@ -4,3 +4,8 @@ export let doSomethingWithRemoteData = somePar => {
   const postData = getPosts();
   return postData.posts.map(it => it.toUpperCase());
 };
+
+export const uncoveredFunction = () => {
+  const postData = getPosts();
+  return postData.posts.map(it => it.toLowerCase());
+}
