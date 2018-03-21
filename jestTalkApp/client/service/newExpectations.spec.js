04aeb79da7291c72c2c5c7782925d61c2257fe0b
Holger Grosse-Plankermann <holgergp@gmail.com>
2018-03-21 17:46:19 +0100

    Moved tests from jestTalkApp here, as they don't belong to the app

--- jestTalkApp/client/service/newExpectations.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Our array should', function () {
-    it('contain someProperties', function () {
-        const someArray = [{property: '1'}, {property: '2', test: true}];
-        expect(someArray).toMatchObject([{property: '1'}, {property: '2'}]);
-    });
-});
--- jestTalkApp/client/util/arrayProducer.js
+++
@@ -1,3 +0,0 @@
- export const arrayProducer = (a) => {
-    return [{first: a, second: '1'},{first: a, second: '2'},{first: a,  second: '2'}]
-}
